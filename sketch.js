8bc92452e5923660ca499034bf0bf1aa2cc6ac6b
Ryan Pavsek <torydee@gmail.com>
2018-03-18 02:11:09 -0400

    Changed how Color works and added additional functions

--- sketch.js
+++ sketch.js
@@ -3,16 +3,18 @@ const w = h = 25; //number of blocks
 let scale = 15; //default scale of blocks on canvas (overwritten in setCanvasSize())
 let blocks = []; //blocks in canvas
 let colors = [[]];  //colors used
-const colorsNum = 10; //number of colors used 
+const colorsNum = 10; //number of colors used
+let userColor = 0; //default color of user
 
 /* Base functions */
-function setup() {
+function setup() { //p5 Function
   setCanvasSize();
   generateColors();
   fillBlocks();
+  userColor = blocks[0][0];
 }
 
-function draw() {
+function draw() { //p5 Function
   setCanvasSize();
   createCanvas(w * scale, h * scale);
   drawBlocks();
@@ -24,7 +26,10 @@ function setCanvasSize() {
 }
 
 /* User input related functions */
+function mouseReleased() { //p5 Function
+  searchBlocks(locToColor(mouseX, mouseY));
 
+}
 
 /* Block related functions */
 function generateColors() {
@@ -38,7 +43,7 @@ function fillBlocks() { //Generate colors with generateColors() before use.
     blocks.push([]);
     blocks.push(new Array(h));
     for (let y = 0; y < h; y++) {
-      blocks[x][y] = colors[Math.floor(Math.random() * colorsNum)];
+      blocks[x][y] = Math.floor(Math.random() * colorsNum);
     }    
   }
 }
@@ -47,9 +52,18 @@ function drawBlocks() { //Must populate blocks with fillBlocks() before use.
   for (let x = 0; x < w; x++) {
     for (let y = 0; y < h; y++) {
       colorMode(RGB, 1);
-      fill(blocks[x][y]);
+      fill(colors[blocks[x][y]]);
       strokeWeight(0);
       rect(x * scale, y * scale, scale, scale);
     }    
   }
+}
+
+function locToColor(x, y) {
+  return blocks[Math.floor(x / scale)][Math.floor(y / scale)];
+}
+/* Logic related functions */
+
+function searchBlocks(selectedColor) {
+  console.log(selectedColor);
 }
